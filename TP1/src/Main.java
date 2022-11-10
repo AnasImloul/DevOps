7f17ab5ef138e468781631bd787205130c2457e0
Abdelhadi-Hit <96614191+Abdelhadi-Hit@users.noreply.github.com>
2022-11-10 10:23:55 +0100

    changed main.java

--- TP1/src/Main.java
+++ TP1/src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         Point<Integer> point1 = new Point<Integer>();
-        Point<Integer> point2 = new Point<Integer>(1, 2);
-        Point<Integer> point3 = new Point<Integer>(1, 2);
-        Point<Integer> point4 = new Point<Integer>(2, 1);
+        Point<Integer> point2 = new Point<Integer>(3, 7);
+        Point<Integer> point3 = new Point<Integer>(4, 5);
+        Point<Integer> point4 = new Point<Integer>(12, 1);
 
         System.out.println(point1);
         System.out.println(point2);
@@ -14,5 +14,8 @@ public class Main {
         System.out.println(point1.equals(point2));
         System.out.println(point2.equals(point3));
         System.out.println(point2.equals(point4));
+        
+        
+        System.out.println("Changed .......");
     }
 }
